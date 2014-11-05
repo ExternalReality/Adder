d92c6cbbbbaec5602acae19e1b23b5092c899dfb
externalreality <ejones@stackbuilders.com>
2014-11-05 10:59:15 -0500

    Full Adder

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,32 @@
 module Main where
 
+import Prelude hiding (and, or)
 
+main :: IO ()
+main = undefined
+
+data Bit = High | Low
+  deriving (Show)
+
+halfAdder :: Bit -> Bit -> (Bit, Bit)
+halfAdder inputOne inputTwo = (inputOne `xor` inputTwo , inputOne `and` inputTwo)
+
+xor :: Bit -> Bit -> Bit
+xor High Low = High
+xor Low High = High
+xor _      _        = Low  
+
+and :: Bit -> Bit -> Bit
+and High High = High
+and _        _       = Low 
+
+fullAdder :: Bit -> Bit -> Bit -> (Bit, Bit)
+fullAdder inputOne inputTwo carryIn = 
+   (sumOutputTwo,  carryOutput `or` carryOutputTwo)     
+  where 
+    (sumOutput, carryOutput)              = halfAdder inputOne inputTwo
+    (sumOutputTwo, carryOutputTwo) = halfAdder sumOutput carryIn
+
+or  :: Bit -> Bit -> Bit
+or  Low Low = Low
+or _        _      = High 
